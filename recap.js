fc01b83c0f442a1ed3b7cd03b7c4727ead0ce199
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-05 02:29:02 +0800

    first commit

---
+++ recap.js
@@ -0,0 +1,27 @@
+/*
+
+1- how to use js with html and css 
+2- get element or elements from dom
+
+2-1: getElementById
+2-2: getElementsByClassName
+2-3: querySelector
+2-4: querySelectorAll 
+
+
+3. dynamically add element to the dom: document.createElement
+ 3-5: set value to the innarHTML
+ 4. get and set value from/to dom element 
+4-1: use innerText (h1, h5, div, span, strong)
+4-2: use value (input, textArea)
+
+
+5. add event lister (event handler)
+5-1: click, focus , blur, onchange, keyup
+2-2: addEventListenet onclick"myHandler()"
+
+
+6. use function to reduce duplicate code
+6. event dubble and evet delegate
+2-1:
+*/
