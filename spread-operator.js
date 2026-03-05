c7eeb7cfdd40fb3a33e1a7610f9a3c42a6b72339
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-06 00:26:01 +0800

    first commit

--- spread-operator.js
+++ spread-operator.js
@@ -1,3 +1,13 @@
-const friends = ["masud", "forhad", "romjan"];
 
-console.log
+// ---------------------------------------------------------------------------
+// const friends = ["masud", "forhad", "romjan"];
+
+// console.log(friends);
+
+
+
+// const friends = ["masud", "forhad", "romjan"];
+
+// console.log(...friends);
+
+// ------------------------------------------------------------------------------------------
