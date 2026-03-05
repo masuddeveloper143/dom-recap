95537848335790f7572d50bd4f6ac1c74b88be17
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-05 23:37:03 +0800

    first commit

--- function.js
+++ function.js
@@ -6,7 +6,11 @@
 // console.log(sum(10, 20));
 
 
-const sum = (num1, num2) => {
-    return num1 + num2;
-};
-console.log(sum(13, 5));
+// const sum = (num1, num2) => {
+//     return num1 + num2;
+// };
+// console.log(sum(13, 5));
+
+const sum = (num1) =>{
+    console.log()
+}
