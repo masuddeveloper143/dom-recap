00162f8c7fe400b67e07c76ca7e5c19c37b37b9a
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-06 01:06:20 +0800

    first commit

---
+++ distructaring/distructaring.js
@@ -0,0 +1,37 @@
+// const hena = {
+//     name: "hena",
+//     age: 10,
+//     friends: ["sena", "kana"],
+//     stuts: "nont found"
+
+// }
+
+// const henaStuts = hena.stuts;
+// const henaName = hena.name;
+// const henaAge = hena.age;
+// const henaFriends = hena.friends;
+
+
+
+
+// const hena = {
+//     name: "hena",
+//     age: 10,
+//     friends: ["sena", "kana"],
+//     stuts: "nont found"
+
+// }
+
+// const {name, age, friends, stuts} = hena;
+
+// console.log(age);
+
+// -------------------------------------------------------------------------------------
+
+
+// const person = ["hero alon", {name:"masud"}, "hena", "bapparaj"];
+
+// const [nyok, obj] = person;
+// console.log(obj);
+
+// ---------------------------------------------------------------------------------
