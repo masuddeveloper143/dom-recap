9d23c062902d06446514f8ff9fe554acba1dbbd3
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-06 11:52:12 +0800

    first commit

--- es6.js
+++ es6.js
@@ -22,3 +22,24 @@
 // person.name = "mahmodul hasan";
 // console.log(person);
 
+// --------------------------------------------------------------------
+
+const person = {
+    name: "masud",
+    age: 23,
+}
+
+const { age, name, } = person;
+
+const friends = ["masud", "banglaseh", "romjan"];
+
+const [frist, second] = friends;
+// console.log(...friends);
+const newFriends = ["hero alam", ...friends];
+
+// console.log(newFriends);
+
+// console.log(`
+//     amar sunar bangla ami tumay valo vashi ${friends[1]} cirodin tumar akash
+//     `)
+
--- object.js
+++ object.js
@@ -20,24 +20,24 @@
 
 
 
-const person = {
-    name: "hena",
-    age: 23,
-    contry: "bd",
-}
-
-// console.log(person["name"]);
-// console.log(person.name);
-
-// for (let key in person ){
-//     console.log(`key : ${key} value: ${person [key]}`)
+// const person = {
+//     name: "hena",
+//     age: 23,
+//     contry: "bd",
 // }
 
-// for (let key of Object.entries(person)) {
-//     console.log(key);
+// // console.log(person["name"]);
+// // console.log(person.name);
+
+// // for (let key in person ){
+// //     console.log(`key : ${key} value: ${person [key]}`)
+// // }
+
+// // for (let key of Object.entries(person)) {
+// //     console.log(key);
+// // }
+
+
+// for (let [key, value] of Object.entries(person)) {
+//     console.log(`key: ${key} value: ${value}`);
 // }
-
-
-for (let [key, value] of Object.entries(person)) {
-    console.log(`key: ${key} value: ${value}`);
-}
