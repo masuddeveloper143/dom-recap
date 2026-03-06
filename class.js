b8140f8c1aa97ada456b6bb35d1c0349c559a5a7
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-06 17:49:38 +0800

    first commit

--- class.js
+++ class.js
@@ -1,14 +1,29 @@
 class product {
     malik = "masud";
 
+    constructor(name, price, brand) {
+
+        this.name = name;
+        this.price = price;
+        this.brandName = brand;
+        this.sordar =this.sordar
+
+        // console.log(name, price,brand);
+
+    }
+
     detles() {
         console.log("ami deailes")
     }
 }
 
+
+const ipone = new product("iphone", "1234", "apple", "joob");
+console.log(ipone);
+
 // const iphone = new product();
 // console.log(iphone.detles);
 
 
-const iphone = new product();
-iphone.detles();
+// const iphone = new product();
+// iphone.detles();
