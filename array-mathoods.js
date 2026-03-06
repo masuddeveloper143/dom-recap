858819f0da214449a866baa264d6feb426e6f0cd
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-06 16:00:10 +0800

    first commit

---
+++ array-mathoods.js
@@ -0,0 +1,8 @@
+const product = [
+    {id: 1, name: "iphone", color: "black", price: 4000, brand: "apple"},
+    {id: 2, name: "iphone", color: "black", price: 4000, brand: "apple"},
+    {id: 3, name: "iphone", color: "black", price: 4000, brand: "apple"},
+    {id: 4, name: "iphone", color: "black", price: 4000, brand: "apple"},
+    {id: 5, name: "iphone", color: "black", price: 4000, brand: "apple"},
+    {id: 6, name: "iphone", color: "black", price: 4000, brand: "apple"},
+]
