5f30d4a7c8a6d48eddff3916ad2d5c9bdece79cb
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-06 17:01:52 +0800

    first commit

--- recap-array-mathoods.js
+++ recap-array-mathoods.js
@@ -1,22 +1,40 @@
-const products = [
-    { id: 1, name: "iphone", color: "black", price: 4000, brand: "apple" },
-    { id: 2, name: "xiaomi", color: "ligthblue", price: 3000, brand: "xiaomi" },
-    { id: 3, name: "samsung", color: "gray", price: 2000, brand: "samsung" },
-    { id: 4, name: "syemphony", color: "blue", price: 1500, brand: "syemphony" },
-    { id: 5, name: "itel", color: "white", price: 1000, brand: "itel" },
-    { id: 6, name: "wolton", color: "green", price: 500, brand: "wolton" },
-];
+// const products = [
+//     { id: 1, name: "iphone", color: "black", price: 4000, brand: "apple" },
+//     { id: 2, name: "xiaomi", color: "ligthblue", price: 3000, brand: "xiaomi" },
+//     { id: 3, name: "samsung", color: "gray", price: 2000, brand: "samsung" },
+//     { id: 4, name: "syemphony", color: "blue", price: 1500, brand: "syemphony" },
+//     { id: 5, name: "itel", color: "white", price: 1000, brand: "itel" },
+//     { id: 6, name: "wolton", color: "green", price: 500, brand: "wolton" },
+// ];
 
 
 
 
+
+// // const singleProduct = products.find(p => p.id === 3);
+// // console.log(singleProduct);
+
+
+// const newProducts2 = products.filter(p => p.name === "iphone");
+// console.log(newProducts2);
+
+
+
+
+
+
+
+// products.forEach(product => console.log(product));
 
 
 // products.forEach(product => {
-//     if(products.brand == "apple"){
+//     if(product.brand=="apple"){
 //         console.log(product)
 //     }
 // });
+
+
+
 
 
 
