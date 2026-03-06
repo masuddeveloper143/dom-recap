62ea875b7d5fc2303f68d64874047f2d8593665d
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-06 10:55:34 +0800

    first commit

---
+++ object.js
@@ -0,0 +1,29 @@
+// const person = {
+//     name: "hena",
+//     age: 10,
+//     country: "bd",
+
+// }
+// Object.seal(person);
+
+// person.stuts="not found";
+
+// person.age = 56;
+
+// console.log(person);
+
+
+
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+// console.log(Object.entries(person));
+
+
+
+const person = {
+    name: "hena",
+    age: 23,
+    contry: "bd",
+}
+
+console.log(person("name"));
