aea28d45c2916c2f634b3ac4dbd35229f5a936db
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-06 16:48:50 +0800

    first commit

---
+++ recap-array-mathoods.js
@@ -0,0 +1,34 @@
+const products = [
+    { id: 1, name: "iphone", color: "black", price: 4000, brand: "apple" },
+    { id: 2, name: "xiaomi", color: "ligthblue", price: 3000, brand: "xiaomi" },
+    { id: 3, name: "samsung", color: "gray", price: 2000, brand: "samsung" },
+    { id: 4, name: "syemphony", color: "blue", price: 1500, brand: "syemphony" },
+    { id: 5, name: "itel", color: "white", price: 1000, brand: "itel" },
+    { id: 6, name: "wolton", color: "green", price: 500, brand: "wolton" },
+];
+
+
+
+
+
+
+// products.forEach(product => {
+//     if(products.brand == "apple"){
+//         console.log(product)
+//     }
+// });
+
+
+
+
+
+// const newProducts = products.map(p => {
+//     if (p.brand === "apple") {
+//         p.price = p.price + 100;
+//     }
+//     return p;
+
+
+// });
+
+// console.log(newProducts);
