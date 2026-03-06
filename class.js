30000ace27bf8c6708fab91cf52c9c6d9d9df836
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-06 17:23:21 +0800

    first commit

---
+++ class.js
@@ -0,0 +1,14 @@
+class product {
+    malik = "masud";
+
+    detles() {
+        console.log("ami deailes")
+    }
+}
+
+// const iphone = new product();
+// console.log(iphone.detles);
+
+
+const iphone = new product();
+iphone.detles();
