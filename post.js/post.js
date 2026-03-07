8cfeabe151ad16a0f9d058209467828bf4149b2e
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-08 01:59:42 +0800

    first commit

--- post.js/post.js
+++ post.js/post.js
@@ -2,12 +2,14 @@ const handlePost = () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(Response => Response.json())
         .then(data =>{
+
+            console.log(data)
             displayPost(data)
         } )
 }
 
-// const displayPost = (posts) =>{
-//     console.log(posts)
-// }
+const displayPost = (posts) =>{
+    console.log(posts)
+}
 
-// handlePost()
+handlePost()
