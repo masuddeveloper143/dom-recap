fdacc451db0a483a60baa2a3f6277121386b646f
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-07 15:51:13 +0800

    first commit

--- index.js/index.js
+++ index.js/index.js
@@ -1,4 +1,26 @@
-const loasUser = (user) => {
-    console.log("clicked button")
-    
+const loasUser = () => {
+    // console.log("clicked button")
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(Response => Response.json())
+
+        .then(data => showUser(data))
+
+    // .then(data =>  {
+    //     // console.log(data[0]);
+    //     // showUser(data)
+
+    // })
+
+}
+
+// const showUser = (users) => {
+//     console.log(users);
+// }
+
+const showUser = (users) => {
+    for (let i = 0; i < users.length; i++) {
+        const user = users[i]
+        console.log(user);
+    }
 }
