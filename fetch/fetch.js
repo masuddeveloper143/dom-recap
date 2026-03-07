c1359876c8e3709ec0e8d5c2415ed45783b465bc
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-07 15:22:37 +0800

    first commit

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -5,14 +5,14 @@
 //     });
 
 
-// const handleLoadData = () => {
-//     // console.log("button cliked");
+const handleLoadData = () => {
+    // console.log("button cliked");
 
 
-//     fetch('https://jsonplaceholder.typicode.com/todos/1')
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log(data);
-//         })
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
 
-// }
+}
---
+++ more-api/more-api.js
@@ -0,0 +1,15 @@
+
+
+const handleLoadUser = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            displayUser(data);
+        })
+}
+
+
+const displayUser = (users) => {
+    console.log(users[5])
+}
