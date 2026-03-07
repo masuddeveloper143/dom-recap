29f578a57ad9da9aa9fe3b87e7ae867774e5df23
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-07 14:51:28 +0800

    first commit

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,10 +1,18 @@
-// fetch('https://jsonplaceholder.typeicode.com/todos/1')
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
 //     .then(res => console.log(res.json()))
 //     .then(data => {
 //         console.log(data);
 //     });
 
 
-const handleLoadData =() => {
-    console.log("button cliked");
+const handleLoadData = () => {
+    // console.log("button cliked");
+
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+
 }
