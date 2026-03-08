11b245a59dbb97a52e3307c7bbc9b9811004030c
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-09 00:35:38 +0800

    first commit

---
+++ asyn.js
@@ -0,0 +1,13 @@
+const handleUser3 = async () => {
+    
+
+    try{
+        const response = fetch("https://jsonplaceholder.typicode.com/users");
+    
+
+
+
+
+        
+    }
+}
--- post.js/post.js
+++ post.js/post.js
@@ -1,35 +1,35 @@
-const handlePost = () => {
+// const handlePost = () => {
 
-    fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(res => res.json())
-        .then(data => {
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//         .then(res => res.json())
+//         .then(data => {
 
-            console.log(data);
-            displayPost(data);
+//             console.log(data);
+//             displayPost(data);
 
-        })
+//         })
 
-}
+// }
 
-const displayPost = (posts) => {
+// const displayPost = (posts) => {
 
-    const postContainer = document.getElementById('postContainer');
+//     const postContainer = document.getElementById('postContainer');
 
-    for (let post of posts) {
+//     for (let post of posts) {
 
-        const div = document.createElement('div');
-        div.classList.add("card");
+//         const div = document.createElement('div');
+//         div.classList.add("card");
 
-        div.innerHTML = `
-        <h1>${post.title}</h1>
-        <p>${post.body}</p>
-        <button>details</button>
-        `;
+//         div.innerHTML = `
+//         <h1>${post.title}</h1>
+//         <p>${post.body}</p>
+//         <button>details</button>
+//         `;
 
-        postContainer.appendChild(div);
+//         postContainer.appendChild(div);
 
-    }
+//     }
 
-}
+// }
 
-handlePost();
+// handlePost();
