4c9f69d1d8fdc71f88498de9d4a729002fe14bb2
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-09 16:41:46 +0800

    first commit

---
+++ eventLoop.js
@@ -0,0 +1,37 @@
+function a() {
+    b();
+    console.log("a");
+}
+
+function b() {
+
+    c();
+    console.log("b");
+}
+
+
+setTimeout(() => {
+    console.log("ami settimeout")
+}, 3000);
+
+
+function c() {
+    d();
+
+    console.log("c");
+}
+
+
+function d() {
+    e();
+
+    console.log("d");
+}
+
+
+function e() {
+    console.log("e")
+}
+
+
+a()
