3f09dbae0f7b5071ca85486507aaf7f612b1e284
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-09 11:26:55 +0800

    first commit

--- timeSet.js
+++ timeSet.js
@@ -1,20 +1,20 @@
-const num1 = 10;
-const num2 = 30;
+// const num1 = 10;
+// const num2 = 30;
 
-console.log("ami prothom ")
-test()
-setTimeout(() => {
-    console.log("ami second function")
-}, 3000)
-
-
-console.log("ami therd ")
-
-const result = num1 + num2;
-
-console.log(result);
+// console.log("ami prothom ")
+// test()
+// setTimeout(() => {
+//     console.log("ami second function")
+// }, 3000)
 
 
-function test(){
-    console.log("ami test");
-}
+// console.log("ami therd ")
+
+// const result = num1 + num2;
+
+// console.log(result);
+
+
+// function test(){
+//     console.log("ami test");
+// }
