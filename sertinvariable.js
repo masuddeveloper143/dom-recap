4eff4eee41f59d6791eda39997bea4345690b466
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-09 16:17:12 +0800

    first commit

--- sertinvariable.js
+++ sertinvariable.js
@@ -1,10 +1,52 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+// setTimeout(() => {
+//     console.log("hello")
+// }, 4000)
+
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+
+
+
+
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+// setInterval(() => {
+//     console.log("hello")
+// }, 3000)
+
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+
+
+
 console.log(1)
 console.log(2)
 console.log(3)
 
-setTimeout(() => {
-    console.log("hello")
-}, 4000)
+let count = 0;
+
+const clockID = setInterval(() => {
+    count++;
+    console.log(count);
+
+    // clearInterval(clockID);
+    if (count >= 10) {
+        clearInterval(clockID);
+        console.log('timeer fhished')
+    }
+
+}, 100)
 
 console.log(4)
 console.log(5)
