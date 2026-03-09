3d9b30f51618107df9b642a51ae44f033fa77d76
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-09 15:50:22 +0800

    first commit

---
+++ async.js
@@ -0,0 +1,21 @@
+const handleData = async () => {
+    console.log("ami prothom")
+    console.log("ami second")
+
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+
+    const data = await response.json();
+
+
+    console.log(data)
+    const result = 10 + 10;
+    console.log(result)
+    console.log('ami therd')
+}
+
+catch {
+    console.log('error')
+}
+
+handleData();
